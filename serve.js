936db940e0c3114bf64e826b4dd9ebe0652d53fc
Shubham Rai <shubham.010793@gmail.com>
2020-08-02 22:02:07 +0530

    Added handling for GET /privacyt

--- serve.js
+++ serve.js
@@ -5,4 +5,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+app.get('/privacy', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'privacy.html'));
+})
 app.listen(8000);
